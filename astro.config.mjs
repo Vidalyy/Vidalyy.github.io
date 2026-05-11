cf3451b4bacc28993067aa765977705d6e9bd928
Peter Czibik <dev@peterczibik.com>
2026-05-11 11:51:07 +0200

    feat: meta tags, cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://vidalyy.github.io',
+  site: 'https://vidalyy.com',
   vite: {
     plugins: [tailwindcss()],
   },
